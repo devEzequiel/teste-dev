89211a6c38e487f5fe3a36880e45fdcab9587554
Ezequiel <ezequiel@jettax.com.br>
2022-09-04 16:59:48 -0300

    [category] - created category's route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+//routes endpoints
+Route::namespace('Api\V1')->name('recipes.')->prefix('v1')->group(function () {
+    require_once ('modules/category.php');
 });
