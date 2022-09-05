052c985b087604e02e4f71d7d62b1652556ff806
Ezequiel <ezequiel@jettax.com.br>
2022-09-05 14:58:25 -0300

    [category] - changed the repsonse message

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -26,7 +26,7 @@ class  CategoryController extends Controller
             $categories = $this->categoryService->list();
 
             if (empty($categories)) {
-                return $this->responseAccepted('Categoria removida');
+                return $this->responseAccepted('Nenhuma categoria encontrada');
             }
 
             return $this->responseOk($categories);
